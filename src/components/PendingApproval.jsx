a8652536ea98b97acf74edce7dbb19f15a077c73
ManuT18 <manuturo2008@gmail.com>
2026-06-23 13:20:23 -0300

    Cambia licencia a CC BY-NC 4.0 y actualiza cabeceras de derechos de autor

--- src/components/PendingApproval.jsx
+++ src/components/PendingApproval.jsx
@@ -1,18 +1,8 @@
 /*
 Copyright (C) 2025 Manuel Tauro
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://gnu.org>.
+This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 */
 
 import React from "react";
